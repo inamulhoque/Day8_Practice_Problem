beda5e7e5470b7c553351fee10f02b49c5e1b44b
Inamul <inamulhoque097@gmail.com>
2022-05-31 15:19:34 +0530

    Added: Part time employee.

--- EmpWage.java
+++ EmpWage.java
@@ -2,12 +2,15 @@ public class EmpWage {
     public static void empWage(){
         int EmpRatePerHour = 20;
         int emphrs=0;
-        int empCheck =(int) Math.floor(Math.random() * 10) % 2;
+        int empCheck =(int) Math.floor(Math.random() * 10) % 3;
         if (empCheck==0) {
             System.out.println("Employee is absent.");
         }
-        else {
-            System.out.println("Employee is present.");
+        else if (empCheck==1){
+            System.out.println("Employee is present part-time.");
+            emphrs=4;
+        } else {
+            System.out.println("Employee is present full-time.");
             emphrs=8;
         }
         int totalWage = EmpRatePerHour*emphrs;
