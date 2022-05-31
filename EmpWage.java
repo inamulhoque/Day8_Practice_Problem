9c5d0eacfe3461792c6df6addd3cec3f99775aa2
Inamul <inamulhoque097@gmail.com>
2022-05-31 15:52:42 +0530

    Added: Maximum working hour is added.

--- EmpWage.java
+++ EmpWage.java
@@ -1,14 +1,14 @@
 public class EmpWage {
     public static void empWage(){
+        int Max_Working_Hour=100;
         int Max_Working_Day = 20;
         int Total_Working_Hour = 0;
         int EmpRatePerHour = 20;
         int emphrs=0;
-        int day = 1;
+        int day = 0;
         int totalWage = 0;
 
-        while (day<=Max_Working_Day) {
-            day++;
+        while (day<Max_Working_Day && Total_Working_Hour<Max_Working_Hour) {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3+1;
             switch (empCheck) {
                 case 1:
@@ -24,10 +24,12 @@ public class EmpWage {
                     emphrs = 0;
                     break;
             }
+            day++;
             Total_Working_Hour=Total_Working_Hour+emphrs;
             totalWage = EmpRatePerHour*Total_Working_Hour;
         }
-        System.out.print("Monthly wage of the employee is: "+totalWage);
+        System.out.println("Monthly wage of the employee is: "+totalWage);
+        System.out.println("Total Working Hour is: "+Total_Working_Hour);
 
     }
     public static void main(String[] args) {
