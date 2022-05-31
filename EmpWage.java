9f9987aadf41e1181afd61c2bed098dd86b66b75
Inamul <inamulhoque097@gmail.com>
2022-05-31 15:10:08 +0530

    Added: Employee is absent or present.

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,15 @@
 public class EmpWage {
+    public static void empWage(){
+        int empCheck =(int) Math.floor(Math.random() * 10) % 2;
+        if (empCheck==0) {
+            System.out.println("Employee is absent.");
+        }
+        else {
+            System.out.println("Employee is present.");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program on master branch.");
+        empWage();
     }
 }
