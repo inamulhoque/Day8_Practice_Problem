1639896e19125039c843968997542ea5ca81fe0f
Inamul <inamulhoque097@gmail.com>
2022-05-31 15:40:36 +0530

    Added: Solved using Switch Case

--- EmpWage.java
+++ EmpWage.java
@@ -1,24 +1,34 @@
 public class EmpWage {
     public static void empWage(){
+        int Max_Working_Day = 20;
+        int Total_Working_Hour = 0;
         int EmpRatePerHour = 20;
         int emphrs=0;
-        int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck){
-            case 1:
-                System.out.println("Employee is present full time");
-                emphrs = 8;
-                break;
-            case 2:
-                System.out.println("Employee is present part time");
-                emphrs = 4;
-                break;
-            case 3:
-                System.out.println("Employee is absent");
-                emphrs=0;
-                break;
+        int day = 1;
+        int totalWage = 0;
+
+        while (day<=Max_Working_Day) {
+            day++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3+1;
+            switch (empCheck) {
+                case 1:
+                    System.out.println("Employee is present full time");
+                    emphrs = 8;
+                    break;
+                case 2:
+                    System.out.println("Employee is present part time");
+                    emphrs = 4;
+                    break;
+                case 3:
+                    System.out.println("Employee is absent");
+                    emphrs = 0;
+                    break;
+            }
+            Total_Working_Hour=Total_Working_Hour+emphrs;
+            totalWage = EmpRatePerHour*Total_Working_Hour;
         }
-        int totalWage = EmpRatePerHour*emphrs;
-        System.out.print("Daily wage is: "+totalWage);
+        System.out.print("Monthly wage is: "+totalWage);
+
     }
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program on master branch.");
